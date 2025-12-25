7ea7946da9d14b52f92922ef55ce51d8802e93bc
rharithra <rharithra9@gmail.com>
2025-12-26 01:07:00 +0530

    Fix: Add missing import for Employee entity in EmployeeRepository

--- src/main/java/com/salaryapp/repository/EmployeeRepository.java
+++ src/main/java/com/salaryapp/repository/EmployeeRepository.java
@@ -1,5 +1,6 @@
 package com.salaryapp.repository;
 
+import com.salaryapp.model.Employee;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
